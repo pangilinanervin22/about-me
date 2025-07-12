41a9c2f59b63043f2373fdad6eda302357d5e56c
Ervin Pangilinan <pangilinanervin22@gmail.com>
2025-07-13 02:04:48 +0800

    Remove dependency on @astrojs/vercel from package.json

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,11 +3,6 @@ import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import react from '@astrojs/react';
-import vercelStatic from '@astrojs/vercel/static';
-
-
-// https://astro.build/config
-import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
@@ -15,7 +10,6 @@ export default defineConfig({
   base: "portfolio",
   integrations: [mdx(), sitemap(), react()],
   output: "static",
-  adapter: vercelStatic({}),
 });
 
 
